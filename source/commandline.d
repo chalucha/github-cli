6e758df4c7a1f5ef6b66cea848975237cd3f7415
Tomáš Chaloupka <chalucha@gmail.com>
2015-08-28 20:39:29 +0200

    fix

--- source/commandline.d
+++ source/commandline.d
@@ -27,7 +27,7 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  */
-module github.commandline;
+module commandline;
 
 import std.array;
 import std.exception;
